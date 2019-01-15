f1ded2c861cea3609f19729e76382042fa9d514b
freestyler@734@gmail.com <freestyler734@gmail.ru>
2019-01-15 23:11:57 +0300

    chapter2

---
+++ src/com/company/designPatternsAndPrinciples/patterns/Singleton.java
@@ -0,0 +1,43 @@
+package com.company.designPatternsAndPrinciples.patterns;
+
+/*
+  Реализация синглтона
+ */
+public class Singleton {
+
+    private String name;
+    private int age;
+
+    // Созданный экземпляр(можно создавать в статическом блоке инициализации)
+    private static final Singleton instance = new Singleton();
+
+    /*
+     Скрываем конструктор и делаем класс фактически final
+     */
+    private Singleton() {
+        name = "Singleton";
+        age = 22;
+    }
+
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // synchronized, т.к. синглтон может использоваться разными потоками
+    public synchronized void addYear(int i) {
+
+        if (i <= 0) {
+            throw new IllegalArgumentException("Число д.б. больше нуля");
+        }
+
+        this.age += i;
+    }
+}
