1ce7b5b8bf6158452bc4f917ec7cd09b1e88df37
iudutov <i.dutov@q.center>
2019-01-25 12:54:22 +0300

    'chapter3'

---
+++ src/com/company/genericsAndCollections/collections/MapInterface.java
@@ -0,0 +1,46 @@
+package com.company.genericsAndCollections.collections;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapInterface {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        // Осн. методы интерфейса Map:
+        System.out.println("map.isEmpty(): " + map.isEmpty());
+        System.out.println("map.size(): " + map.size());
+        System.out.println("map.clear(): "); map.clear(); // void
+        System.out.println("");
+        System.out.println("map.put(\"First\", 1): " + map.put("First", 1)); // возвращает предыдущее значение при замене
+        System.out.println("map.put(\"Second\", 2): " + map.put("Second", 2)); // возвращает предыдущее значение при замене
+        System.out.println("map.put(\"Third\", 3): " + map.put("Third", 3)); // возвращает предыдущее значение при замене
+        System.out.println("map.put(\"First\", 10): " + map.put("First", 10)); // возвращает предыдущее значение при замене
+        System.out.println("");
+        System.out.println("map.get(new String(\"Second\")): " + map.get(new String("Second"))); // использует equals. null, если пусто
+        System.out.println("");
+        System.out.println("map.remove(\"Third\"): " + map.remove("Third")); // null, если пусто
+        System.out.println("");
+        System.out.println("map.containsKey(\"Third\"): " + map.containsKey("Third")); // return boolean
+        System.out.println("map.containsValue(10): " + map.containsValue(10)); // return boolean
+        System.out.println("");
+        Set<String> keys = map.keySet();
+        Collection<Integer> values = map.values();
+
+        for (String key: map.keySet()) {
+            System.out.print(map.get(key)+ " ");
+        }
+        System.out.println("");
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            entry.setValue(entry.getValue() * 10);
+            System.out.println(entry.getKey()+": " + entry.getValue());
+        }
+        System.out.println("");
+        for (String key: map.keySet()) {
+            System.out.print(map.get(key)+ " ");
+        }
+
+    }
+}
--- src/com/company/genericsAndCollections/collections/QueueInterface.java
+++ src/com/company/genericsAndCollections/collections/QueueInterface.java
@@ -1,21 +1,36 @@
 package com.company.genericsAndCollections.collections;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Queue;
 
 public class QueueInterface {
 
     public static void main(String[] args) {
         Queue<String> queue = new ArrayDeque<>();
-        Queue<String> stack = new ArrayDeque<>();
+        Deque<String> stack = new ArrayDeque<>();
         // основные методы интерфейса Queue, которых нет в Collection
         // queue
-        System.out.println("queue.add(\"Str1\"): " + queue.add("Str1"));
-        System.out.println("queue.add(\"Str2\"): " + queue.add("Str2"));
-        System.out.println("queue.add(\"Str3\"): " + queue.add("Str3"));
+        System.out.println("queue.add(\"Str1\"): " + queue.add("Str1")); // return boolean
+        System.out.println("queue.add(\"Str2\"): " + queue.add("Str2")); // return boolean
+        System.out.println("queue.add(\"Str3\"): " + queue.add("Str3")); // return boolean
         System.out.println("");
-        System.out.println("queue.element(): " + queue.element());
-        System.out.println("queue.offer(\"Str4\"): " + queue.offer("Str4"));
-
+        System.out.println("queue.element(): " + queue.element()); // NoSuchElementException, если пусто
+        System.out.println("queue.offer(\"Str4\"): " + queue.offer("Str4")); // return boolean
+        System.out.println("queue.remove(): " + queue.remove()); // NoSuchElementException, если пусто
+        System.out.println("queue.poll(): " + queue.poll()); // null, если пусто
+        System.out.println("queue.peek(): " + queue.peek()); // null, если пусто
+        System.out.println("----------------");
+        // stack
+        stack.push("Str1"); // void
+        stack.push("Str2"); // void
+        stack.push("Str3"); // void
+        stack.push("Str4"); // void
+        System.out.println("");
+        System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
+        System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
+        System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
+        System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
+        //System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
     }
 }
