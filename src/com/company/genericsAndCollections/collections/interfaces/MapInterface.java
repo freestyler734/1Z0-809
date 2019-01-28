ae38d23aa929fdd7cdce38720413cfe56d470580
iudutov <i.dutov@q.center>
2019-01-28 17:23:25 +0300

    'chapter3'

--- src/com/company/genericsAndCollections/collections/interfaces/MapInterface.java
+++ src/com/company/genericsAndCollections/collections/interfaces/MapInterface.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class MapInterface {
 
@@ -54,7 +56,38 @@ public class MapInterface {
 
 
         /// Java 8 Map Api ///
-        map.putIfAbsent("Tenth", 100);
+        System.out.println(" map.putIfAbsent(\"Tenth\", 100): " +  map.putIfAbsent("Tenth", 100));
+        System.out.println(" map.putIfAbsent(\"Tenth\", 100): " +  map.putIfAbsent("Tenth", 10));
+        System.out.println(" map.putIfAbsent(\"Eleventh\", null): " +  map.putIfAbsent("Eleventh", null));
+        System.out.println(map);
+        System.out.println("");
+        // Пример использования: V merge() - возвращает значение по ключу, которое осталось после изменений, либо новое значение
+        BiFunction<Integer, Integer, Integer> mergeFunc = (v1, v2) -> v1 > v2 ? v1 : v2;
+        BiFunction<Integer, Integer, Integer> mergeFuncNull = (v1, v2) -> null; // случай когда BiFunction возвращает null
+        System.out.println("map.merge(\"Second\", 30, mergeFunc): "  + map.merge("Second", 30, mergeFunc)); // ключ есть - присваиваем значение по логике BiFunction
+        System.out.println("map.merge(\"Second\", 50, mergeFunc): " + map.merge("Second", 50, mergeFunc)); // ключ есть - присваиваем значение по логике BiFunction
+        System.out.println("map.merge(\"Third\", 1, mergeFunc): " + map.merge("Third", 1, mergeFunc)); // если ключ новый - добавляеется значение без применения BiFunction
+        //System.out.println("map.merge(\"Forth\", null, mergeFunc): " + map.merge("Forth", null, mergeFunc)); // если ключ новый - и значение null - NullPointerException
+        //System.out.println("map.merge(\"First\", null, mergeFunc): " + map.merge("First", null, mergeFunc)); // ключ есть - и значение null - NullPointerException
+        System.out.println("map.put(\"Forth\", null): " + map.put("Forth", null));
+        System.out.println("map.merge(\"Forth\", 111, mergeFunc): " +  map.merge("Forth", 111, mergeFunc)); // если старое значение null, то значение просто заменяется и BiFunction не вызывается
+        System.out.println(map.merge("First", 222, mergeFuncNull)); // если BiFunction вернула null, то если ключ существовал - он удалаяется (возвращает значение новое)
+        System.out.println(map.merge("Fifth", 222, mergeFuncNull)); // если BiFunction вернула null, если ключ новый - добавляеется значение без применения BiFunction
+        System.out.println(map);
+        System.out.println("");
+        // Примеры использования computeIfPresent/computeIfAbscent - возвращ-т новое значение, иначе null
+        BiFunction<String, Integer, Integer> presentComputer = (k, v) -> v + 1;
+        System.out.println("map.computeIfPresent(\"First\", presentComputer): " + map.computeIfPresent("First", presentComputer));
+        System.out.println("map.computeIfPresent(\"Second\", presentComputer): " + map.computeIfPresent("Second", presentComputer));
+        System.out.println("map.computeIfPresent(\"Third\", (k,v) -> null): " + map.computeIfPresent("Third", (k,v) -> null)); // ключ удаляется из map
+        System.out.println(map);
+        System.out.println("");
+        Function<String, Integer> abscentComputer = (k) -> 12;
+        System.out.println("map.computeIfAbsent(\"First\", abscentComputer): " + map.computeIfAbsent("First", abscentComputer));
+        System.out.println("map.computeIfAbsent(\"Third\", abscentComputer): " + map.computeIfAbsent("Third", abscentComputer));
+        System.out.println("map.put(\"Seventh\", null): " + map.put("Seventh", null));
+        System.out.println("map.computeIfAbsent(\"Seventh\", abscentComputer): " + map.computeIfAbsent("Seventh", abscentComputer));
+        System.out.println("map.computeIfAbsent(\"Eighth\", (k) -> null): " + map.computeIfAbsent("Eighth", (k) -> null)); // ключ не добавится с null
         System.out.println(map);
     }
 }
