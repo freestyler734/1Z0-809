c82e7f2ac89d65fb81522e81923ca843348670cc
iudutov <i.dutov@q.center>
2019-02-07 18:52:40 +0300

    'chapter3'

--- src/com/company/genericsAndCollections/collections/interfaces/QueueInterface.java
+++ src/com/company/genericsAndCollections/collections/interfaces/QueueInterface.java
@@ -15,18 +15,27 @@ public class QueueInterface {
         System.out.println("queue.add(\"Str1\"): " + queue.add("Str1")); // return boolean
         System.out.println("queue.add(\"Str2\"): " + queue.add("Str2")); // return boolean
         System.out.println("queue.add(\"Str3\"): " + queue.add("Str3")); // return boolean
+        System.out.println(queue);
         System.out.println("");
         System.out.println("queue.element(): " + queue.element()); // NoSuchElementException, если пусто
         System.out.println("queue.offer(\"Str4\"): " + queue.offer("Str4")); // return boolean
+        System.out.println(queue);
         System.out.println("queue.remove(): " + queue.remove()); // NoSuchElementException, если пусто
+        System.out.println(queue);
         System.out.println("queue.poll(): " + queue.poll()); // null, если пусто
+        System.out.println(queue);
         System.out.println("queue.peek(): " + queue.peek()); // null, если пусто
         System.out.println("----------------");
         // stack
+        System.out.println("stack.push(\"Str1\"): ");
+        System.out.println("stack.push(\"Str2\"): ");
+        System.out.println("stack.push(\"Str3\"): ");
+        System.out.println("stack.push(\"Str4\"): ");
         stack.push("Str1"); // void
         stack.push("Str2"); // void
         stack.push("Str3"); // void
         stack.push("Str4"); // void
+        System.out.println(stack);
         System.out.println("");
         System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
         System.out.println("stack.pop(): " + stack.pop());// NoSuchElementException, если пусто
